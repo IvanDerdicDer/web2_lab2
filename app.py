ace4bff96ddca154f52f16b47b026ffefbdbfd5f
IvanDerdicDer <46329317+IvanDerdicDer@users.noreply.github.com>
2022-11-06 17:46:35 +0100

    XSS

--- app.py
+++ app.py
@@ -1,11 +1,81 @@
-from flask import Flask
+from datetime import datetime
+from enum import Enum, auto
+from functools import wraps
+from itertools import count
+from os import environ as env
+from typing import Generator
+from urllib.parse import quote_plus, urlencode
+
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, redirect, render_template, session, url_for, jsonify, request
+
+comments_list = ['<script>alert("XSS attack has happened")</script>']
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
 
 app = Flask(__name__)
+app.secret_key = env.get("APP_SECRET_KEY")
+
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route("/")
+def home():
+    return render_template("home.html", session=session.get('user'), comments=comments_list)
+
+
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://" + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("home", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
+
+
+@app.post('/comments')
+def comments():
+    return render_template('comments.html', comments=comments_list, is_safe=request.form.get('is_safe'))
+
+
+@app.post('/add_comment')
+def add_comment():
+    comments_list.append(request.form.get('comment'))
+    return redirect('/comments')
 
 
 if __name__ == '__main__':
