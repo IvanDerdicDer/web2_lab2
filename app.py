0f5a3fb2673ba39603bd5a72901454a90002d34f
IvanDerdicDer <46329317+IvanDerdicDer@users.noreply.github.com>
2022-11-06 20:51:44 +0100

    Broken Auth

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ from dotenv import find_dotenv, load_dotenv
 from flask import Flask, redirect, render_template, session, url_for, jsonify, request
 
 comments_list = ['<script>alert("XSS attack has happened")</script>']
+break_or_make_var = None
+xss_toggle = None
+auth_users = ['auth0|6356ed1d5615e6a1bdb56435', 'auth0|6356ee6ae584359a2df8303a', 'auth0|6356ed948d3ef662e0286c6d']
 
 ENV_FILE = find_dotenv()
 if ENV_FILE:
@@ -32,9 +35,24 @@ oauth.register(
 )
 
 
+def authenticate() -> bool:
+    if 'user' not in session:
+        return False
+
+    user_id = session['user']['userinfo']['sub']
+
+    return user_id in auth_users
+
+
 @app.route("/")
 def home():
-    return render_template("home.html", session=session.get('user'), comments=comments_list)
+    return render_template(
+        "home.html",
+        session=session.get('user'),
+        comments=comments_list,
+        xss_toggle=xss_toggle,
+        is_auth=authenticate()
+    )
 
 
 @app.route("/login")
@@ -67,15 +85,37 @@ def logout():
     )
 
 
-@app.post('/comments')
-def comments():
-    return render_template('comments.html', comments=comments_list, is_safe=request.form.get('is_safe'))
-
-
 @app.post('/add_comment')
 def add_comment():
     comments_list.append(request.form.get('comment'))
-    return redirect('/comments')
+    return redirect('/')
+
+
+@app.post('/toggle_xss')
+def toggle_xss():
+    global xss_toggle
+    xss_toggle = request.form.get('is_safe')
+    return redirect('/')
+
+
+@app.post('/break_or_make')
+def break_or_make():
+    global break_or_make_var
+    break_or_make_var = request.form.get('is_safe')
+
+    return redirect('/')
+
+
+@app.get('/delete_comments')
+def delete_comments():
+    if break_or_make_var:
+        if not authenticate():
+            return 'Unauthorized'
+
+    while comments_list:
+        comments_list.pop()
+
+    return redirect('/')
 
 
 if __name__ == '__main__':
